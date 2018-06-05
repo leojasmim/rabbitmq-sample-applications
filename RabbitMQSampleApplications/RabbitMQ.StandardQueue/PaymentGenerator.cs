86690025d11c30095fbb6ad5704e58fb4302e98b
leojasmim <leojasmim@hotmail.com>
2018-06-05 20:20:47 -0300

    :construction: Cria PaymentManager
    Cria classe de gerenciamento de pagamentos

---
+++ RabbitMQSampleApplications/RabbitMQ.StandardQueue/PaymentGenerator.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Common;
+using RandomNameGeneratorLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQ.StandardQueue
+{
+    public static class PaymentManager
+    {
+        public static readonly string BIN = "41112222";
+        public static readonly int MIN_CARD_NUMBER = 10000000;
+        public static readonly int MAX_CARD_NUMBER = 99999999;
+
+        public static void ShowPayment(Payment payment) {
+            Console.WriteLine($"Payment => Name: {payment.HolderName},Card: {payment.CardNumber}, Amount: {payment.AmountToPay}");
+        }
+
+        public static List<Payment> CreatePayments(int numberOfPayments) {
+
+            var personGenerator = new PersonNameGenerator();
+
+            List<Payment> payments = new List<Payment>();
+            Random random = new Random();
+
+            for (int i = 0; i < numberOfPayments; i++) {
+
+                decimal amount = random.Next(1, 100);
+                string card = BIN + random.Next(MIN_CARD_NUMBER, MAX_CARD_NUMBER).ToString();
+                var name = personGenerator.GenerateRandomFirstAndLastName();
+
+                payments.Add(new Payment() { AmountToPay = amount, CardNumber = card, HolderName = name });
+            }
+
+            return payments;
+        }
+    }
+}
