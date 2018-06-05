64666a3e743af4b6fb309c2eebdb24bf4adf8d91
leojasmim <leojasmim@hotmail.com>
2018-06-05 19:29:58 -0300

    :construction: Cria modelos
    Cria as classes comuns a todos projetos

---
+++ RabbitMQSampleApplications/RabbitMQ.Common/Payment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQ.Common
+{
+    [Serializable]
+    public class Payment
+    {
+        public decimal AmountToPay { get; set; }
+        public string CardNumber { get; set; }
+        public string HolderName { get; set; }
+    }
+}
---
+++ RabbitMQSampleApplications/RabbitMQ.Common/PurchaseOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQ.Common
+{
+    public class PurchaseOrder
+    {
+        public decimal AmountToPay { get; set; }
+        public string PONumber { get; set; }
+        public string CompanyName { get; set; }
+        public int PaymentDaysTerms { get; set; }
+    }
+}
